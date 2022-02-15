10fd3d7f200b07e16cd3d6d5ad3873e36e8fbbcb
DrokoDomi <droko@gmx.de>
2022-02-15 23:10:15 +0100

    Fixed STATICFILES_DIRS for Heroku

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -158,11 +158,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    'static/',
+    os.path.join(PROJECT_ROOT, "static/")
 ]
 
 # Default primary key field type
